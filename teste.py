fa766f0907ed554076313eaa603b9d1ed84b2adb
Elber Lopes <elbercaio@gmail.com>
2021-09-28 23:44:03 -0300

    Checar se o espaço é valido e se perdemos o jogo

--- teste.py
+++ teste.py
@@ -1,5 +1,10 @@
 grid = [[(0, 0, 0) for _ in range(10)] for _ in range(20)]
+#
+# for y, linha in enumerate(grid):
+#     for x, coluna in enumerate(linha):
+#         print(coluna)
 
-for y, linha in enumerate(grid):
-    for x, coluna in enumerate(linha):
-        print(coluna)
+accepted_pos = [(j, i) for j in range(10) for i in range(20)
+                if grid[i][j] == (0, 0, 0)]
+
+print(len(accepted_pos))
